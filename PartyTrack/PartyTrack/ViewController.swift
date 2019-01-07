e40f3f4a3d8d979772fb27deb8bbfdd085302ee7
Cecilia Osuji (s5067418) <s5067418@mac42933.student.bournemouth.ac.uk>
2019-01-07 15:07:23 +0000

    c
    set up location manager Delegate

--- PartyTrack/PartyTrack/ViewController.swift
+++ PartyTrack/PartyTrack/ViewController.swift
@@ -12,9 +12,18 @@ class ViewController: UIViewController {
         
         locationManager.requestWhenInUseAuthorization()
         
+        locationManager.delegate = self
+        locationManager.startUpdatingLocation()
+        
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
 }
 
+
+extension ViewController: CLLocationManagerDelegate {
+    
+}
