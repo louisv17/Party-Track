a9e11d0c72b46a27eb52f7d6c66f5d5ed32c45c1
Cecilia Osuji (s5067418) <s5067418@mac38573.student.bournemouth.ac.uk>
2019-01-08 15:33:40 +0000

    c
    updates

--- PartyTrack/ClubManager.swift
+++ PartyTrack/ClubManager.swift
@@ -13,9 +13,28 @@ class ClubManager {
         clubs.append(cameoClub)
         
         
+        let haloCoordinate = CLLocationCoordinate2DMake(50.7193855, -1.882557)
+        let haloClub = Club(coordinate: haloCoordinate, title: "Halo Nightclub")
+        clubs.append(haloClub)
+        
+        let firestationCoordinate = CLLocationCoordinate2DMake(50.7230995, -1.8671757)
+        let firestationClub = Club(coordinate: firestationCoordinate, title: "The Old Firestation")
+        clubs.append(firestationClub)
+        
+        let lostCoordinate = CLLocationCoordinate2DMake(50.7180103, -1.8821358)
+        let lostClub = Club(coordinate: lostCoordinate, title: "The Lost Paradise")
+        clubs.append(lostClub)
+    
+        
+        
+        func setActivateClub(title: String)
+    
+    
+   
     
     
     
     
+    }
 }
-}
+
